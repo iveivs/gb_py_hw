d04a9aea0fa5e8612fdc19796c3a69e6a212900c
veiv <veiv@mail.ru>
2022-09-28 17:01:26 +0400

    add task1

--- hwpy1/task1.py
+++ hwpy1/task1.py
@@ -1,1 +1,12 @@
-input_day = input
+input_day_num = int(input("Укажите номер дня недели: "))
+
+def kind_of_days(number):
+    week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+    if number < 1 or number > 7:
+            print('Вы указали некорректное значение')
+    elif number < 6:
+        print(f' {week[number - 1]} - будний день, не выходной!')
+    else:
+        print(f' {week[number - 1]} - выходной день!')
+
+kind_of_days(input_day_num)
