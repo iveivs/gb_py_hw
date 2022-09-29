a895c5b657e25814bff79568c7bd711afd785a70
veiv <veiv@mail.ru>
2022-09-29 19:28:44 +0400

    add hw1_task_2

---
+++ hw1_task_2/hw1_task_2.py
@@ -0,0 +1,14 @@
+
+x = int(input("Введите x: "))
+y = int(input("Введите y: "))
+
+if x > 0 and y > 0:
+        print("Первая четверть")
+elif x > 0 and y < 0:
+        print("Вторая четверть")
+elif x < 0 and y < 0:
+        print("Третья четверть")
+elif x < 0 and y > 0:
+        print("Четвёртая четверть")
+
+
