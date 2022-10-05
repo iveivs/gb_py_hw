12bb0706cda5fb65f9469f70cc2b68f828a2c2a7
veiv <veiv@mail.ru>
2022-10-05 23:38:13 +0400

    add solution hw3_task_5

---
+++ hw3_task_5/hw3_task_5.py
@@ -0,0 +1,13 @@
+# Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+
+def fib(n):
+    print(n)
+    if n == 0:
+        return 0
+    if n in [1, 2]:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+
+print(fib(8))
