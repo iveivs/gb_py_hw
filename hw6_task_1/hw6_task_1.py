620d364fe281029600a139edfdafe4afb51cd699
veiv <veiv@mail.ru>
2022-10-18 03:18:52 +0400

    hw6_task_1

---
+++ hw6_task_1/hw6_task_1.py
@@ -0,0 +1,10 @@
+# Напишите прогр. вычисления арифм. выражения заданного строкой.
+
+str_inp1 = '2 + 2'
+print(f'{str_inp1} = {eval(str_inp1)}')
+
+str_inp2 = '1 + 2 * 3'
+print(f'{str_inp2} = {eval(str_inp2)}')
+
+str_inp3 = '1 - 2 * 3'
+print(f'{str_inp3} = {eval(str_inp3)}')
